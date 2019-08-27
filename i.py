500941e15c05ec24bbf57a8b200ab1adc22ca594
Pavithra-srii <54575810+Pavithra-srii@users.noreply.github.com>
2019-08-27 14:26:18 +0530

    Add files via upload

---
+++ i.py
@@ -0,0 +1,4 @@
+dic={'pavi':[6,8,4],'tara':{'c':[45,78],'n':[78]},'srii':[4]}
+
+print(dic.values())
+
---
+++ ii.py
@@ -0,0 +1,4 @@
+name=input("my name is")
+cur_age=int(input("current age is"))
+mar_age=int(input("I want to marriage at my age"))
+print("I am{} and my current age is {}and I want marriage in{}".format(name,cur_age,mar_age+2019-cur_age))
